f71fb731c557dc334c345470bebaffe16a924ef4
Justine Edward Santos <hydxenonmain@gmail.com>
2024-06-06 18:41:19 +0800

    Refactor login layout

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,17 +1,11 @@
-import LoginForm from "@/components/auth/login-form";
+import LoginForm from "@/components/auth/LoginForm";
 import AuthFormWrapper from "@/components/auth/auth-form-wrapper";
 import React from "react";
 
 const LoginPage = () => {
   return (
-    <div className="flex items-center justify-center py-8 px-4">
-      <AuthFormWrapper
-        title="login"
-        backBtnRef="/register"
-        backBtnLabel="Don't have an account? Click here to register."
-      >
-        <LoginForm />
-      </AuthFormWrapper>
+    <div>
+      <LoginForm />
     </div>
   );
 };
---
+++ components/auth/auth-nav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthNav = () => {
+  return (
+    <nav>
+      <h1 className="text-3xl text-[#5AA46F] px-8 py-8">PROJECT E-SIP</h1>
+    </nav>
+  );
+};
+
+export default AuthNav;
