71180e9d69625634b93287a40df0ca094b0f9d27
Justine Edward Santos <hydxenonmain@gmail.com>
2024-08-20 19:23:11 +0800

    Change color

--- components/(users)/student/Header.tsx
+++ components/(users)/student/Header.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 const Header = ({ children, className }: HeaderProps) => {
   return (
-    <div className="min-h-[92px] mt-2 flex-nowrap text-black flex w-full items-center justify-between gap-2 px-4">
+    <div className="min-h-[50px] mt-2 flex-nowrap text-black flex w-full items-center justify-between gap-2 px-4">
       <div className="md:flex-1">
         <Image
           src={"/assets/icons/logo.svg"}
